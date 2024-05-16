5d0ed2e1ca576339c4b3e6b55f6ecb1534854e8c
Arjun <arjun346002@gmail.com>
2024-05-16 12:47:11 +0530

    create some features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SignupPage from './pages/SignupPage';
+import Cart from './features/cart/Cart';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage/> ,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage/> ,
+  },
+  {
+    path: "/cart",
+    element: <Cart/> ,
+  },
+]);
+
+
 
 function App() {
   return (
     <div>
-      <Home/>
+       <RouterProvider router={router} />
     </div>
   );
 }

b6b967a9a20beb53eb9776ef7b270ee3e784f72d
Arjun <arjun346002@gmail.com>
2024-05-20 21:16:45 +0530

    complete checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -29,40 +29,40 @@ const products = [
   // More products...
 ];
 
-const people = [
-    {
-      name: 'Leslie Alexander',
-      street: '11th main',
-      city: 'Delhi',
-      state : "Delhi",
-      pinCode: 120932,
-      phone : 9828191271
-    },
-    {
-        name: 'Alexander',
-        street: '12th main',
-        city: 'Banglore',
-        state : "Karnatka",
-        pinCode: 50332,
-        phone : 90922191271
-      },
-      {
-        name: 'Leslie',
-        street: '20th main',
-        city: 'Lucknow',
-        state : "Up",
-        pinCode: 110332,
-        phone : 9273191271
-      },
-      {
-        name: 'Lesjka',
-        street: '34th main',
-        city: 'Mohali',
-        state : "Punjab",
-        pinCode: 112932,
-        phone : 9298191271
-      },
-  ]
+const Addresses = [
+  {
+    name: "Leslie Alexander",
+    street: "11th main",
+    city: "Delhi",
+    state: "Delhi",
+    pinCode: 120932,
+    phone: 9828191271,
+  },
+  {
+    name: "Alexander",
+    street: "12th main",
+    city: "Banglore",
+    state: "Karnatka",
+    pinCode: 50332,
+    phone: 90922191271,
+  },
+  {
+    name: "Leslie",
+    street: "20th main",
+    city: "Lucknow",
+    state: "Up",
+    pinCode: 110332,
+    phone: 9273191271,
+  },
+  {
+    name: "Lesjka",
+    street: "34th main",
+    city: "Mohali",
+    state: "Punjab",
+    pinCode: 112932,
+    phone: 9298191271,
+  },
+];
 
 const Checkout = () => {
   return (
@@ -70,13 +70,13 @@ const Checkout = () => {
       <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
         <div className="lg:col-span-3 bg-white mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="space-y-12 ">
-            <form className="" >
-              <div className="mt-16 " >
+            <form className="">
+              <div className="mt-16 ">
                 <div className="border-b border-gray-900/10 pb-12">
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">
+                  <h2 className="text-3xl mb-4 font-semibold leading-7 text-gray-900">
                     Personal Information
                   </h2>
-                  <p className="mt-1 text-sm leading-6 text-gray-600">
+                  <p className="mt-1 text-base leading-6 text-gray-600">
                     Use a permanent address where you can receive mail.
                   </p>
 
@@ -229,41 +229,71 @@ const Checkout = () => {
                     </div>
                   </div>
                 </div>
+                <div className="mt-6 mb-5 flex items-center justify-end gap-x-6">
+                <button
+                  type="button"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Reset
+                </button>
+                <button
+                  type="submit"
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Save Address
+                </button>
+              </div>
 
                 <div className="border-b border-gray-900/10 pb-12">
                   <h2 className="text-base mt-2 font-semibold leading-7 text-gray-900">
-                    Address
+                  Addresses
                   </h2>
                   <p className="mt-1 text-sm leading-6 text-gray-600">
                     Choose from existing Address
                   </p>
                   <div>
-                  <ul role="list" className="divide-y divide-gray-100">
-      {people.map((person) => (
-        <li key={person.email} className="flex justify-between gap-x-6 py-5">
-          <div className="flex min-w-0 gap-x-4">
-            <div className="min-w-0 flex-auto">
-              <p className="text-base font-semibold leading-6 text-gray-900">{person.name}</p>
-              <p className=" text-sm mt-1  truncate leading-5 text-gray-500">{person.state}</p>
-              <p className=" text-sm   mt-1 truncate leading-5 text-gray-500">{person.city}</p>
-            </div>
-          </div>
-          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm leading-6 text-gray-900">PinCode :  {person.pinCode}</p>
-            {person.lastSeen ? (
-              <p className="mt-1 text-xs leading-5 text-gray-500">
-                Ph No. {person.phone}
-              </p>
-            ) : (
-              <div className="mt-1 flex items-center gap-x-1.5">
-                Street <p className="text-sm font-semibold leading-6 text-gray-900">{person.street}</p>
-
-              </div>
-            )}
-          </div>
-        </li>
-      ))}
-    </ul>
+                    <ul role="list" className="divide-y divide-gray-100">
+                      {Addresses.map((person) => (
+                        <li
+                          key={person.email}
+                          className="flex justify-between gap-x-6 py-5"
+                        >
+                          <div className="flex min-w-0 gap-x-4">
+                          <input
+                            name="people"
+                            type="radio"
+                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                          />
+                            <div className="min-w-0 flex-auto">
+                              <p className="text-base font-semibold leading-6 text-gray-900">
+                                {person.name}
+                              </p>
+                              <p className=" text-sm mt-1  truncate leading-5 text-gray-500">
+                                {person.state}
+                              </p>
+                              <p className=" text-sm   mt-1 truncate leading-5 text-gray-500">
+                                {person.city}
+                              </p>
+                            </div>
+                          </div>
+                          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                          <p className="mt-1 font-semibold text-sm leading-5 text-gray-900">
+                                Ph No. {person.phone}
+                              </p>
+                            <p className="text-sm leading-6 text-gray-900">
+                              PinCode : {person.pinCode}
+                            </p>
+                              
+                              <div className="mt-1 flex items-center gap-x-1.5">
+                                <p className="text-sm font-semibold leading-6 text-gray-900">
+                                  {person.street}
+                                </p>
+                              </div>
+                            
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
                   </div>
 
                   <div className="mt-10 space-y-10">
@@ -309,112 +339,98 @@ const Checkout = () => {
                 </div>
               </div>
 
-              <div className="mt-6 mb-5 flex items-center justify-end gap-x-6">
-                <button
-                  type="button"
-                  className="text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Save
-                </button>
-              </div>
+              
             </form>
           </div>
-          
         </div>
         <div className="lg:col-span-2">
-            <div className="mx-auto bg-white  max-w-7xl px-4 sm:px-6 lg:px-8">
-              <div className="mt-10">
-                <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                  <h2 className="text-3xl my-5 font-semibold">Cart</h2>
-                  <hr />
-                  <ul role="list" className="my-4 divide-y divide-gray-200">
-                    {products.map((product) => (
-                      <li key={product.id} className="flex py-6">
-                        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                          <img
-                            src={product.imageSrc}
-                            alt={product.imageAlt}
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-
-                        <div className="ml-4 flex flex-1 flex-col">
-                          <div>
-                            <div className="flex justify-between text-base font-medium text-gray-900">
-                              <h3>
-                                <a href={product.href}>{product.name}</a>
-                              </h3>
-                              <p className="ml-4">{product.price}</p>
-                            </div>
-                            <p className="mt-1 text-sm text-gray-500">
-                              {product.color}
-                            </p>
-                          </div>
-                          <div className="flex flex-1 items-end justify-between text-sm">
-                            <div className="text-gray-500">
-                              <label
-                                htmlFor="password"
-                                className="block text-sm font-medium leading-6 text-gray-900"
-                              >
-                                Qty
-                              </label>
-                            </div>
-
-                            <div className="flex">
-                              <button
-                                type="button"
-                                className="font-medium text-indigo-600 hover:text-indigo-500"
-                              >
-                                Remove
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-
+          <div className="mx-auto bg-white  max-w-7xl px-0 sm:px-0 lg:px-0">
+            <div className="mt-10">
               <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                <div className="flex justify-between text-base font-medium text-gray-900">
-                  <p>Subtotal</p>
-                  <p>$262.00</p>
-                </div>
-                <p className="mt-0.5 text-sm text-gray-500">
-                  Shipping and taxes calculated at checkout.
+                <h2 className="text-3xl my-5 font-semibold">Cart</h2>
+                <hr />
+                <ul role="list" className="my-4 divide-y divide-gray-200">
+                  {products.map((product) => (
+                    <li key={product.id} className="flex py-6">
+                      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                        <img
+                          src={product.imageSrc}
+                          alt={product.imageAlt}
+                          className="h-full w-full object-cover object-center"
+                        />
+                      </div>
+
+                      <div className="ml-4 flex flex-1 flex-col">
+                        <div>
+                          <div className="flex justify-between text-base font-medium text-gray-900">
+                            <h3>
+                              <a href={product.href}>{product.name}</a>
+                            </h3>
+                            <p className="ml-4">{product.price}</p>
+                          </div>
+                          <p className="mt-1 text-sm text-gray-500">
+                            {product.color}
+                          </p>
+                        </div>
+                        <div className="flex flex-1 items-end justify-between text-sm">
+                          <div className="text-gray-500">
+                            <label
+                              htmlFor="password"
+                              className="block text-sm font-medium leading-6 text-gray-900"
+                            >
+                              Qty
+                            </label>
+                          </div>
+
+                          <div className="flex">
+                            <button
+                              type="button"
+                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                            >
+                              Remove
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+              <div className="flex justify-between text-base font-medium text-gray-900">
+                <p>Subtotal</p>
+                <p>$262.00</p>
+              </div>
+              <p className="mt-0.5 text-sm text-gray-500">
+                Shipping and taxes calculated at checkout.
+              </p>
+              <div className="mt-6">
+                <a
+                  href="/"
+                  className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                >
+                  Pay and Order
+                </a>
+              </div>
+              <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                <p>
+                  or
+                  <Link to={"/"}>
+                    <button
+                      type="button"
+                      className="font-medium mx-2 text-indigo-600 hover:text-indigo-500"
+                    >
+                      Continue Shopping
+                      <span aria-hidden="true"> &rarr;</span>
+                    </button>
+                  </Link>
                 </p>
-                <div className="mt-6">
-                  <a
-                    href="/"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                  >
-                    Checkout
-                  </a>
-                </div>
-                <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                  <p>
-                    or
-                    <Link to={"/"}>
-                      <button
-                        type="button"
-                        className="font-medium mx-2 text-indigo-600 hover:text-indigo-500"
-                      >
-                        Continue Shopping
-                        <span aria-hidden="true"> &rarr;</span>
-                      </button>
-                    </Link>
-                  </p>
-                </div>
               </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );

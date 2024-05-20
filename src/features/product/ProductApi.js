1ab201205ade31f5d2828d5e3060d57be6b5be19
Arjun <arjun346002@gmail.com>
2024-05-20 22:52:42 +0530

    fetch Api and store data in redux

--- src/features/product/ProductApi.js
+++ src/features/product/ProductApi.js
@@ -1,7 +1,6 @@
-// A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-    const res = await fetch("https://localhost:8080");
+    const res = await fetch("http://localhost:8080/products");
     const data = await res.json();
     resolve({data});
   });

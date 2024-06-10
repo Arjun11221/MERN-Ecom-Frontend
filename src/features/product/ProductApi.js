e2ee23c73f842e99c1b72dc0223f396995da57b7
Arjun <arjun346002@gmail.com>
2024-06-10 22:50:55 +0530

    divide sections

--- src/features/product/ProductApi.js
+++ src/features/product/ProductApi.js
@@ -6,11 +6,17 @@ export function fetchAllProducts() {
   });
 }
 
-export function fetchProductsByFilter(filter) {
+export function fetchProductsByFilter(filter, sort) {
   let queryString = '';
-  for(let key in filter){
-    queryString += `${key} = ${filter[key]}&`;
-  } 
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      queryString += `${key}=${categoryValues}&`;
+    }
+  }
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
   return new Promise(async (resolve) => {
     const res = await fetch("http://localhost:8080/products?"+queryString);
     const data = await res.json();
